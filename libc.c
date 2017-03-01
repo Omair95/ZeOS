a5ffe9b37839a6830286af9e183c6d90567f458b
Omair <Omair7@protonmail.ch>
2017-03-01 23:03:48 +0100

    First commit

---
+++ libc.c
@@ -0,0 +1,73 @@
+/*
+ * libc.c 
+ */
+
+#include <libc.h>
+
+#include <types.h>
+
+int errno;
+
+void itoa(int a, char *b)
+{
+  int i, i1;
+  char c;
+  
+  if (a==0) { b[0]='0'; b[1]=0; return ;}
+  
+  i=0;
+  while (a>0)
+  {
+    b[i]=(a%10)+'0';
+    a=a/10;
+    i++;
+  }
+  
+  for (i1=0; i1<i/2; i1++)
+  {
+    c=b[i1];
+    b[i1]=b[i-i1-1];
+    b[i-i1-1]=c;
+  }
+  b[i]=0;
+}
+
+int strlen(char *a)
+{
+  int i;
+  
+  i=0;
+  
+  while (a[i]!=0) i++;
+  
+  return i;
+}
+
+
+int write(int fd, char * buffer, int size)
+{
+	int eax;
+	__asm__("int $0x80 \n\t"
+		: "=a" (eax)
+		: "b" (fd), "c" (buffer), "d" (size));
+	if (eax < 0) {
+		errno = -eax;
+		return -1;
+	}
+	errno = 0;
+	return eax;
+}
+
+int gettime() {
+	int eax;
+	__asm__("int $0x80 \n\t"
+		: "=a" (eax));
+	if (eax < 0) {
+		errno = -eax;
+		return -1;
+	}
+	errno = 0;
+	return eax;
+}
+
+
---
+++ sys.c
@@ -0,0 +1,68 @@
+/*
+ * sys.c - Syscalls implementation
+ */
+#include <devices.h>
+
+#include <utils.h>
+
+#include <io.h>
+
+#include <mm.h>
+
+#include <mm_address.h>
+
+#include <sched.h>
+
+#include <system.h>
+
+#define LECTURA 0
+#define ESCRIPTURA 1
+
+
+int check_fd(int fd, int permissions)
+{
+  if (fd!=1) return -9; /*EBADF*/
+  if (permissions!=ESCRIPTURA) return -13; /*EACCES*/
+  return 0;
+}
+
+int sys_ni_syscall()
+{
+	return -38; /*ENOSYS*/
+}
+
+int sys_getpid()
+{
+	return current()->PID;
+}
+
+int sys_fork()
+{
+  int PID=-1;
+
+  // creates the child process
+  
+  return PID;
+}
+
+void sys_exit()
+{  
+}
+
+int sys_write(int fd, char * buffer, int size) 
+{
+   int f = check_fd(fd, ESCRIPTURA);
+
+   if (f == 0 && buffer != NULL && size > 0) {
+	int s = sys_write_console(buffer, size);
+	return s;
+   }
+   return 0;
+}
+
+int sys_gettime() {
+   return 0;
+}
+
+
+
